42d1b57ec4b9c18237bc2e1eb779f4643b617747
Youssef Al-Kobary <168835912+youssef1alkobary@users.noreply.github.com>
2024-05-10 08:51:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
+// let menuBar = document.querySelector('#menuBar');
+
+// menuBar.onclick = function menu() {
+//     let list = document.querySelector('#list');
+//     let menuX = document.querySelector('#menuX');
+//     this.style.display = "none";
+//     list.style.display = "block";
+//     menuX.style.display = "block";
+// }
+
+// let menuX = document.querySelector('#menuX');
+
+// menuX.onclick = function menu() {
+//     let list = document.querySelector('#list');
+//     let menuX = document.querySelector('#menuX');
+//     menuBar.style.display = "block";
+//     list.style.display = "none";
+//     menuX.style.display = "none";
+// }
+
 let menuBar = document.querySelector('#menuBar');
 
-menuBar.onclick = function menu() {
-    let list = document.querySelector('#list');
+menuBar.onclick = function menubar() {
+    let lists = document.querySelector('#list');
     let menuX = document.querySelector('#menuX');
+
     this.style.display = "none";
-    list.style.display = "block";
     menuX.style.display = "block";
+    lists.style.marginLeft = "-35px";
 }
 
 let menuX = document.querySelector('#menuX');
 
-menuX.onclick = function menu() {
-    let list = document.querySelector('#list');
-    let menuX = document.querySelector('#menuX');
+menuX.onclick = function menuX() {
+    let lists = document.querySelector('#list');
+
+    this.style.display = "none";
     menuBar.style.display = "block";
-    list.style.display = "none";
-    menuX.style.display = "none";
+    lists.style.marginLeft = "-400px";
 }
